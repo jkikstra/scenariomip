0f19621795fc145a60dc8bde22faed00596354b1
jkikstra <kikstra@iiasa.ac.at>
2025-03-14 11:46:39 +0100

    Only keep main downloading option

--- R/download_scenarios.R
+++ R/download_scenarios.R
@@ -32,56 +32,6 @@ print(conn_ssp.scenarios())
 ")
 }
 
-# All models; by model ----
-
-# Define the Python code to retrieve the scenario
-py_run_string("
-import pyam
-import os
-
-# Establish connection to the legacy pyam platform 'ssp-submission'
-pyam.iiasa.Connection('ssp_submission')
-conn_ssp = pyam.iiasa.Connection('ssp_submission')
-models = conn_ssp.models()
-
-# Retrieve the desired scenarios (download everything; loop over model-scenario combinations to not blow up RAM)
-for m in models:
-  df = pyam.read_iiasa(
-      'ssp_submission',
-      model=m
-  )
-
-  # Write out the scenario data
-  output_file = 'scenarios_scenariomip' + m + '.csv'
-  df.to_csv(
-      os.path.join('data','downloading_models',output_file)
-  )
-  print(f'Data has been written to {output_file}')
-
-")
-
-# Recombine the data files from each model into one file
-SCENARIO.FILES.FOLDER <- here("data", "downloading_models")
-FILES.csv <- file.path(SCENARIO.FILES.FOLDER, dir(SCENARIO.FILES.FOLDER, pattern = "*.csv"))  # get file names
-scenarios_csv <- FILES.csv %>%
-  map(~ (load_csv_iamc(.) %>% iamc_wide_to_long()) ) %>%
-  reduce(rbind) %>%
-  drop_na() %>%
-  arrange(Model, Scenario, Region, Unit, year) %>%
-  iamc_long_to_wide()
-iamc_cols <- c("Model", "Scenario", "Region", "Variable", "Unit")
-df_cols <- scenarios_csv %>% colnames() %>% sort
-year_cols_ordered <- setdiff(df_cols, iamc_cols)
-sorted_iamc <- c(iamc_cols, year_cols_ordered)
-scenarios_csv_o <- scenarios_csv %>% select(all_of(sorted_iamc))
-
-write_delim(
-  x = scenarios_csv,
-  file = here("data", paste0("scenarios_scenariomip_allmodels_", Sys.Date(),".csv")),
-  delim = ","
-)
-
-
 # All model-scenario combinations; by properties ----
 
 # Define the Python code to retrieve the scenario
@@ -97,6 +47,7 @@ props = conn_ssp.properties().reset_index()
 # Retrieve the desired scenarios (download everything; loop over model-scenario combinations to not blow up RAM)
 for index, (m, s) in enumerate(zip(props['model'], props['scenario'])):
 
+  # N.B. if the code breaks somewhere, and you want to continue after the first N scenarios, uncomment the below and replace 96 with N
   # if index < 96:
   #   continue
 
@@ -122,8 +73,9 @@ for index, (m, s) in enumerate(zip(props['model'], props['scenario'])):
 # Recombine the data files from each model into one file
 SCENARIO.FILES.FOLDER <- here("data", "downloading_iters")
 FILES.csv <- file.path(SCENARIO.FILES.FOLDER, dir(SCENARIO.FILES.FOLDER, pattern = "*.csv"))  # get file names
+FILES.csv <- FILES.csv[!grepl(FILES.csv, pattern="MESSAGEix-GLOBIOM 2.1-M-R12",fixed=T)]
 scenarios_csv <- FILES.csv %>%
-  map(~ (load_csv_iamc(.) %>% iamc_wide_to_long()) ) %>%
+  map(~ (load_csv_iamc(.) %>% iamc_wide_to_long() %>% filter(year<=2100) ) ) %>%
   reduce(rbind) %>%
   drop_na() %>%
   arrange(Model, Scenario, Region, Unit, year) %>%
@@ -134,16 +86,25 @@ year_cols_ordered <- setdiff(df_cols, iamc_cols)
 sorted_iamc <- c(iamc_cols, year_cols_ordered)
 scenarios_csv_o <- scenarios_csv %>% select(all_of(sorted_iamc))
 
+# delete some unnecessary data (or data that otherwise needs to be removed)
+scenarios_csv_o <- scenarios_csv_o %>% filter(
+  # !(Model=="AIM 3.0" & Scenario=="SSP1 - High Emissions"),
+  # !(Model=="AIM 3.0" & Scenario=="SSP2 - High Emissions"),
+  !(Model=="MESSAGEix-GLOBIOM 2.1-M-R12")
+) %>%
+  select(-c(`2105`,`2110`,`2115`,`2120`,`2125`,`2130`,`2135`,`2140`,`2145`,`2150`)) %>%
+  colnames()
+
 write_delim(
-  x = scenarios_csv,
-  file = here("data", paste0("scenarios_scenariomip_allmodels_", Sys.Date(),".csv")),
+  x = scenarios_csv_o,
+  file = here("data", paste0("scenarios_scenariomip_allmodels_", Sys.Date(),"_b.csv")),
   delim = ","
 )
 
-scenarios_csv_message <- scenarios_csv %>% filter(grepl(Model, pattern="MESSAGE", fixed=T))
+scenarios_csv_message <- scenarios_csv_o %>% filter(grepl(Model, pattern="MESSAGE", fixed=T))
 write_delim(
   x = scenarios_csv_message,
-  file = here("data", paste0("scenarios_scenariomip_allmodels_", Sys.Date(),"-message.csv")),
+  file = here("data", paste0("scenarios_scenariomip_allmodels_", Sys.Date(),"-message_b.csv")),
   delim = ","
 )
 
@@ -154,30 +115,3 @@ write_delim(
   file = here("data", paste0("scenarios_floorspace_", Sys.Date(),"-message.csv")),
   delim = ","
 )
-
-
-# Only MESSAGE ----
-
-# Define the Python code to retrieve the scenario (only retrieve MESSAGE)
-py_run_string("
-import pyam
-import os
-
-# Establish connection to the legacy pyam platform 'ssp-submission'
-pyam.iiasa.Connection('ssp_submission')
-conn_ssp = pyam.iiasa.Connection('ssp_submission')
-
-# Retrieve the desired scenarios (download everything)
-df = pyam.read_iiasa(
-    'ssp_submission',
-    model='MESSAGEix-GLOBIOM 2.1-M-R12'
-)
-
-# Write out the scenario data
-output_file = 'scenarios_scenariomip' + 'MESSAGE' + '.csv'
-df.to_csv(
-    os.path.join('data',output_file)
-)
-print(f'Data has been written to {output_file}')
-
-")
